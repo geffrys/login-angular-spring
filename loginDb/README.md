f167920e5c373088137f4449fc12d9c5574469d3
Geffry Alejandro Ospina Atehortúa <geffry.ospina@gmail.com>
2024-08-17 17:21:07 -0500

    base project

---
+++ loginDb/README.md
@@ -0,0 +1,10 @@
+# Initialization
+
+Build the image with the same name defined at deployment file.
+
+```bash
+
+docker build -t <login-mysql>:<latest> .
+docker run -d -p 3308:3306 --name login-mysql -e MYSQL_ROOT_PASSWORD=root login-mysql:0.1
+
+```
---
+++ loginDb/ddl-schema.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA loginDb;
+USE loginDb;
+
+CREATE TABLE tbl_user (
+    user_id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    user_username VARCHAR(50) NOT NULL,
+    user_password VARCHAR(50) NOT NULL,
+    user_email VARCHAR(50) NOT NULL,
+    user_fullname VARCHAR(50) NOT NULL,
+    user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
